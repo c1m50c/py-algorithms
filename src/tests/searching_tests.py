73c237f6b26522746f382aa345c0de8ed966d6fd
c1m50c <terminalityms@gmail.com>
2021-10-12 19:56:20 -0500

    searching_tests documentation

--- src/tests/searching_tests.py
+++ src/tests/searching_tests.py
@@ -7,19 +7,39 @@ from time import time
 
 
 def get_color(found: int) -> Fore:
+    """
+        # Get Color
+        Returns Red or Green Foreground color depending on if the item was found within the array.
+        ### Parameters:
+        ```py
+        found: int # Return value from a searching algorithm.
+        ```
+    """
     if found <= -1:
         return Fore.RED
     return Fore.GREEN
 
 
 def speed_test(array: list[any], method: Callable[[list[any], any], int], sorting_method: Callable[[list[int]], None] = merge_sort, length: int = 100) -> None:
+    """
+        # Speed Test
+        Tests a searching algorithm's speed by searching through an array of size `length`.
+        ### Parameters:
+        ```py
+        array: list[any] # Array to search, will be created upon function call with `create_integer_array`.
+        method: Callable[[list[any], any], int] # Searching Algorithm method.
+        sorting_method: Callable[[list[int]], None] = merge_sort # Method for sorting the array, used when a Searching Algorithm requires the array to be sorted.
+        length: int = 100 # Determines the length of the array when created.
+        ```
+    """
     print(f"{Style.DIM}Search Speed Test{Style.RESET_ALL}")
     
-    RAND_MIN: int = -10000
-    RAND_MAX: int = 10000
+    RAND_MIN: int = -20000
+    RAND_MAX: int = 20000
     
     create_integer_array(array=array, length=length, rand_min=RAND_MIN, rand_max=RAND_MAX)
     if method.__name__ in ["binary_search"]:
+        # Sort Array for search algorithms that require sorting.
         print(f"Pre-Sorting Array with {Fore.BLUE}{Style.BRIGHT}'{get_method_name(sorting_method)}'{Fore.RESET}{Style.RESET_ALL}.")
         begin_time: float = time()
         sorting_method(array)
@@ -36,12 +56,22 @@ def speed_test(array: list[any], method: Callable[[list[any], any], int], sortin
 
 
 def run_tests(method: Callable[[list[any], any], int]) -> None:
+    """
+        # Run Tests
+        Runs tests to ensure the searching algorithm method can properly search, it also tests their speed.
+        ### Parameters:
+        ```py
+        method: Callable[[list[any], any], int] # Searching Algorithm Method.
+        ```
+    """
     setrecursionlimit(5000) # Handy to have this higher then default limit, be cautious of crashes when too high though
     print(f"{Style.BRIGHT}{Fore.YELLOW}<====={Fore.CYAN}{get_method_name(method)}{Fore.YELLOW}=====>{Fore.RESET}{Style.RESET_ALL}")
     
     arr: list[int] = [  ]
     
     # Speed Tests
+    speed_test(array=arr, method=method, length=1000)
+    print()
     speed_test(array=arr, method=method, length=10000)
     print()
     speed_test(array=arr, method=method, length=100000)
