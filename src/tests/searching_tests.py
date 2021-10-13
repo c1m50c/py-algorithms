0e4be04262b27972abe75296e8e73c9170bad249
c1m50c <terminalityms@gmail.com>
2021-10-12 19:26:20 -0500

    pretty colors for search tests

--- src/tests/searching_tests.py
+++ src/tests/searching_tests.py
@@ -6,23 +6,32 @@ from .helper_functions import *
 from time import time
 
 
+def get_color(found: int) -> Fore:
+    if found <= -1:
+        return Fore.RED
+    return Fore.GREEN
+
+
 def speed_test(array: list[any], method: Callable[[list[any], any], int], sorting_method: Callable[[list[int]], None] = merge_sort, length: int = 100) -> None:
-    print(f"Search Speed Test")
+    print(f"{Style.DIM}Search Speed Test{Style.RESET_ALL}")
     
     RAND_MIN: int = -1000
     RAND_MAX: int = 1000
     
-    # Array Length of 10000 Test #
     create_integer_array(array=array, length=length, rand_min=RAND_MIN, rand_max=RAND_MAX)
     if method.__name__ in ["binary_search"]:
-        print(f"Pre-Sorting Array with '{get_method_name(sorting_method)}'.")
+        print(f"Pre-Sorting Array with {Fore.BLUE}{Style.BRIGHT}'{get_method_name(sorting_method)}'{Fore.RESET}{Style.RESET_ALL}.")
+        begin_time: float = time()
         sorting_method(array)
-    print(f"Array Length: '{len(array)}'.")
+        end_time: float = time()
+        print(f"{Fore.GREEN}Sorted Array in {end_time - begin_time}s{Fore.RESET}")
+    
+    print(f"{Fore.YELLOW}Array Length: {Style.BRIGHT}'{len(array)}'{Style.NORMAL}.{Fore.RESET}")
     begin_time: float = time()
     finding: int = randint(a=RAND_MIN, b=RAND_MAX)
     found = method(array, finding)
     end_time: float = time()
-    print(f"Searched for '{finding}', found at index '{found}'.")
+    print(f"{get_color(found=found)}Searched for {Style.BRIGHT}'{finding}'{Style.NORMAL}, found at index {Style.BRIGHT}'{found}'{Style.RESET_ALL}{Fore.RESET}.")
     print(f"Time Elapsed: {end_time - begin_time}s")
 
 
@@ -32,7 +41,9 @@ def run_tests(method: Callable[[list[any], any], int]) -> None:
     
     arr: list[int] = [  ]
     
-    # Speed Test
+    # Speed Tests
     speed_test(array=arr, method=method, length=10000)
+    print()
+    speed_test(array=arr, method=method, length=100000)
     
     print(f"{Style.BRIGHT}{Fore.YELLOW}<==========>{Fore.RESET}{Style.RESET_ALL}")
